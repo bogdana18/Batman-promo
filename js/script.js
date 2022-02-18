c6aa1375e62f13d447747c30f829357b2641bbcc
bogdana18 <usenkobogdana19@gmail.com>
2022-02-18 22:49:10 +0200

    added menu movement

--- js/script.js
+++ js/script.js
@@ -1,20 +1,36 @@
-const sliderThumbs = new Swiper('.slider-thumbs', {
-  loop: true,
-  spaceBetween: 20,
-  slidesPerView: 3,
-  centeredSlides: true,
-  loopedSlides: 4,
-});
+const burger = document.querySelector('.burger')
+const navigation = document.querySelector('.navigation');
+const navigationClose = document.querySelector('.navigation__close');
 
-sliderThumbs.on('click', (swiper) => {
-  swiper.slideTo(swiper.clickedIndex)
+burger.addEventListener('click', () => {
+  navigation.classList.add('navigation_active');
 })
 
-const sliderMain = new Swiper('.slider-main', {
-  loop: true,
-  spaceBetween: 10,
-  loopedSlides: 4,
-});
+navigationClose.addEventListener('click', () => {
+  navigation.classList.remove('navigation_active');
+})
 
-sliderThumbs.controller.control = sliderMain;
-sliderMain.controller.control = sliderThumbs;
+try {
+  const sliderThumbs = new Swiper('.slider-thumbs', {
+    loop: true,
+    spaceBetween: 20,
+    slidesPerView: 3,
+    centeredSlides: true,
+    loopedSlides: 4,
+  });
+
+  sliderThumbs.on('click', (swiper) => {
+    swiper.slideTo(swiper.clickedIndex)
+  })
+
+  const sliderMain = new Swiper('.slider-main', {
+    loop: true,
+    spaceBetween: 10,
+    loopedSlides: 4,
+  });
+
+  sliderThumbs.controller.control = sliderMain;
+  sliderMain.controller.control = sliderThumbs;
+} catch {
+  console.log('На этой странице нет слайдера')
+}
